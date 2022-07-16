4a8737097fe4fe1a0019892362061d41333d0ea7
fangyan <fangyan@qunhemail.com>
2022-07-16 21:51:46 +0800

    update anchor and pivot

--- src/examples/006_anchor.ts
+++ src/examples/006_anchor.ts
@@ -1,8 +1,8 @@
 /**
  * Sprite.anchor: 锚点
- * anchor 的值为 sprite 的高度和宽度的比例值，0.5 表示 0.5w 和 0.5h
- * 表示设置 Sprite 的原点（Sprite 内部的局部坐标系的原点），默认原点为左上角
- * Sprite 旋转、缩放、定位等都是以 anchor 为基准的
+ * 1.DisplayObject.position 设置了 DisplayObject 内部的局部坐标系的原点，决定了其 rotate、scale、skew 的中心点
+ * 2.anchor 是 Sprite 特有的属性，用于设置 texture（即图片）在 sprite 上的位置。默认 anchor.x = 0， anchor.y = 0，
+ * 表示纹理的左上角在 Sprite.position 位置，anchor 为 1（x 和 y 都是 1）表示纹理的右下角在 Sprite.position 位置
  */
 import * as PIXI from "pixi.js";
 import reactjpg from "../assests/react.jpg";
@@ -14,8 +14,8 @@ document.body.appendChild(app.view);
 let sprite = PIXI.Sprite.from(reactjpg);
 sprite.width = 200;
 sprite.height = 200;
-sprite.position.set(200, 200);
-sprite.anchor.set(0.5);
+sprite.position.set(200, 200); // position 决定了内部的坐标系原点
+sprite.anchor.set(1);
 
 let childSprite = PIXI.Sprite.from(bgRotate);
 // 这里设置的是相对于父元素 sprite 的大小
@@ -23,12 +23,11 @@ let childSprite = PIXI.Sprite.from(bgRotate);
 // 因为设置 了 sprite 尺寸为 200，所以最终 childSprite 的尺寸为 100
 childSprite.width = 152;
 childSprite.height = 152;
-// childSprite.position.set()
 sprite.addChild(childSprite);
 
 app.stage.addChild(sprite);
 
-
 app.ticker.add((delta) => {
-    childSprite.rotation += delta * 0.05;
+    // childSprite.rotation += delta * 0.05;
+    sprite.rotation += delta * 0.05;
 });
--- src/examples/007_pivot.ts
+++ src/examples/007_pivot.ts
@@ -1,5 +1,14 @@
 /**
- * DisplayObject.pivot 与 Sprite.anchor 类似，只不过使用的是像素值
+ * DisplayObject.pivot 设置的是物体的定位点，也是该物体旋转围绕的中心点，默认为左上角点。但是它不改变物体内部的坐标系，
+ * 即物体内部的坐标系原点始终为左上角点。
+ * 
+ * 例如有一个 DisplayObject 类型的 obj：
+ * obj.height = 400;
+ * obj.width = 400;
+ * obj.position.set(100, 100);
+ * obj.pivot.set(200)
+ * 最终的结果是物体的中心点位于其 parent 局部坐标系的 (100, 100)，并且 obj 的旋转中心为它的中心点
+ * 但是设置 pivot 并未改变它自己的局部坐标系，即 obj 的局部坐标系的原点依然在 obj 的左上角点。
  */
 import * as PIXI from "pixi.js";
 import bgRotate from "../assests/bg_rotate.jpg";
@@ -13,22 +22,15 @@ document.body.appendChild(app.view);
 
 const container = new PIXI.Container();
 app.stage.addChild(container);
-console.log("containerHeight", container.getBounds().height);
-console.log("containerWidth", container.getBounds().width);
 
 const sprite = PIXI.Sprite.from(bgRotate);
 sprite.width = spriteWidth;
 sprite.height = spriteHeight;
-// 下面两句代码等价，0.5 设置的是比例值，因为使用纹理时，你可能不清楚纹理的真实像素大小
-// 而 375 正是纹理的像素大小的一半
-sprite.anchor.set(0.5);
-// sprite.pivot.set(375, 375);
+sprite.position.set(200, 200);
 container.addChild(sprite);
 container.position.set(width * 0.5, height * 0.5);
-// container.pivot.set(container.getBounds().width * 0.5, container.getBounds().height * 0.5);
-console.log("ContainerBounds", container.getBounds());
-console.log("ContainerPosition", container.position);
+container.pivot.set(400, 400);
 app.ticker.add((delta) => {
-    sprite.rotation += delta * 0.05;
+    // sprite.rotation += delta * 0.05;
     // container.rotation += delta * 0.05;
 });
